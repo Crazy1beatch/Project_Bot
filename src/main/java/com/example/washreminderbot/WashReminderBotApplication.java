d179c7dc55283e0d0c0ab9ba6a004914e402a6a4
zhora <zhora.shevelev196@mail.ru>
2022-09-27 23:43:56 +0500

    Starting

---
+++ src/main/java/com/example/washreminderbot/WashReminderBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.washreminderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashReminderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashReminderBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/washreminderbot/WashReminderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.washreminderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashReminderBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
